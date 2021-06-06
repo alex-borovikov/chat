5d217933b939a69f016e9b5fb1c4c7a5dbfddb27
0x d83f <alex.borrovikov@gmail.com>
2021-06-06 12:23:28 +0300

    set formik and yup validation for sign in form || check email field || check password field

--- client/src/Auth/Login/Login.jsx
+++ client/src/Auth/Login/Login.jsx
@@ -5,15 +5,23 @@ import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import Textfield from "../../TextField/Textfield";
 import clsx from "clsx";
 import useStyles from "./Login.styles";
+import useStylesReg from "../Register/Register.styles";
+
+import { Formik } from "formik";
+import * as yup from "yup";
 
 const Login = () => {
     const classes = useStyles();
+    const classesReg = useStylesReg();
     const [checked, setChecked] = useState(false);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
     const [type, setType] = useState('password');
 
-    const handleChange = (event) => {
+    const validationSchema = yup.object().shape({
+        email: yup.string().email().typeError('Email is required field').required(),
+        password: yup.string().required('No password provided'),
+    })
+
+    const handleChangeFlag = (event) => {
         setChecked(event.target.checked);
         type === 'password' ? setType('text') : setType('password')
     };
@@ -47,78 +55,95 @@ const Login = () => {
                         <div className={classes.form__header}>
                             <h2>Sign in</h2>
                         </div>
-                        <form className={classes.form_control}>
-                            <div className={classes.form__section}>
-                                <div className='form__section-item form__section-label'>
-                                    <label htmlFor="emailAdress">Email adress</label>
+                        <Formik
+                            onSubmit={async values => {
+                                await new Promise(resolve => setTimeout(resolve, 500));
+                                alert(JSON.stringify(values, null, 2));
+                            }}
+                            initialValues={{
+                                email: '',
+                                password: ''
+                            }}
+                            validationSchema={validationSchema}
+                        >{({values, errors, touched, handleChange, handleBlur, isValid, handleSubmit, dirty}) =>(
+                            <form className={classes.form_control}>
+                                <div className={classes.form__section}>
+                                    <div className='form__section-item form__section-label'>
+                                        <label htmlFor="emailAdress">Email adress</label>
+                                    </div>
+                                    <div className='form__section-item '>
+                                        <Textfield
+                                            placeholder='Enter your email'
+                                            value={values.email}
+                                            onChange={handleChange}
+                                            type='email'
+                                            id='emailAdress'
+                                            className={classes.textfield}
+                                            autoComplete={'on'}
+                                            tabIndex='1'
+                                            name='email'
+                                        />
+                                        {touched.email && errors.email && <p className={classesReg.error}>{errors.email}</p>}
+                                    </div>
                                 </div>
-                                <div className='form__section-item '>
-                                    <Textfield
-                                        placeholder='Enter your email'
-                                        value={email}
-                                        onChange={e => setEmail(e.target.value)}
-                                        type='email'
-                                        id='emailAdress'
-                                        className={classes.textfield}
-                                        autoComplete={'on'}
-                                        tabIndex='1'
+                                <div className={classes.form__section}>
+                                    <div className='form__section-item form__section-label'>
+                                        <label htmlFor="password">Password</label>
+                                    </div>
+                                    <div className='form__section-item '>
+                                        <Textfield
+                                            placeholder='Enter password'
+                                            value={values.password}
+                                            onChange={handleChange}
+                                            type={type}
+                                            id='password'
+                                            className={classes.textfield}
+                                            tabIndex='2'
+                                            name='password'
+                                        />
+                                        {touched.password && errors.password && <p className={classesReg.error}>{errors.password}</p>}
+                                    </div>
+                                </div>
+                                <div className={clsx(classes.form__section, classes.form__section_showPassword)}>
+                                    <Checkbox
+                                        checked={checked}
+                                        onChange={handleChangeFlag}
+                                        id='checkbox'
                                     />
+                                    <label htmlFor='checkbox' className='form__section-label__text'>Show password</label>
                                 </div>
-                            </div>
-                            <div className={classes.form__section}>
-                                <div className='form__section-item form__section-label'>
-                                    <label htmlFor="password">Password</label>
-                                </div>
-                                <div className='form__section-item '>
-                                    <Textfield
-                                        placeholder='Enter password'
-                                        value={password}
-                                        onChange={e => setPassword(e.target.value)}
-                                        type={type}
-                                        id='password'
-                                        className={classes.textfield}
-                                        tabIndex='2'
-                                    />
-                                </div>
-                            </div>
-                            <div className={clsx(classes.form__section, classes.form__section_showPassword)}>
-                                <Checkbox
-                                    checked={checked}
-                                    onChange={handleChange}
-                                    id='checkbox'
-                                />
-                                <label htmlFor='checkbox' className='form__section-label__text'>Show password</label>
-                            </div>
-                            <div className={clsx(classes.form__section, classes.form__sectionMargin)}>
-                                <Button variant="contained" className='form__section-signin'>
-                                    <span>Sign up</span>
-                                    <ArrowForwardIcon className={classes.arrow} />
-                                </Button>
-                            </div>
-                            <div className={clsx(classes.form__section, classes.form__section_parent)}>
-                                <span className={classes.form__section_divider} />
-                                <span>or</span>
-                                <span className={classes.form__section_divider} />
-                            </div>
-                            <div className={clsx(classes.form__section, classes.form__btn)}>
-                                <Button variant="outlined">
-                                    <img alt='google_logo.png' src="https://img.icons8.com/office/16/000000/google-logo.png"/>
-                                    <span>Continue with google</span>
-                                </Button>
-                            </div>
-                            <div className={clsx(classes.form__section, classes.form__section_parent, classes.nodisplay)}>
-                                <span className={clsx(classes.form__section_divider, classes.dividerMargin)} />
-                                <span>or</span>
-                                <span className={classes.form__section_divider} />
-                            </div>
-                            <div className={clsx(classes.form__section, classes.form__signup)}>
-                                <a href="/api/signup">
-                                    <Button variant="contained" >
-                                        <span>Sign up</span>
+                                <div className={clsx(classes.form__section, classes.form__sectionMargin)}>
+                                    <Button disabled={!isValid && !dirty} type='submit' onClick={handleSubmit} variant="contained" className='form__section-signin'>
+                                        <span>Sign in</span>
+                                        <ArrowForwardIcon className={classes.arrow} />
                                     </Button>
-                                </a>
-                            </div>
-                        </form>
+                                </div>
+                                <div className={clsx(classes.form__section, classes.form__section_parent)}>
+                                    <span className={classes.form__section_divider} />
+                                    <span>or</span>
+                                    <span className={classes.form__section_divider} />
+                                </div>
+                                <div className={clsx(classes.form__section, classes.form__btn)}>
+                                    <Button variant="outlined">
+                                        <img alt='google_logo.png' src="https://img.icons8.com/office/16/000000/google-logo.png"/>
+                                        <span>Continue with google</span>
+                                    </Button>
+                                </div>
+                                <div className={clsx(classes.form__section, classes.form__section_parent, classes.nodisplay)}>
+                                    <span className={clsx(classes.form__section_divider, classes.dividerMargin)} />
+                                    <span>or</span>
+                                    <span className={classes.form__section_divider} />
+                                </div>
+                                <div className={clsx(classes.form__section, classes.form__signup)}>
+                                    <a href="/api/signup">
+                                        <Button variant="contained" >
+                                            <span>Sign up</span>
+                                        </Button>
+                                    </a>
+                                </div>
+                            </form>
+                        ) }
+                        </Formik>
                     </div>
                 </Box>
             </Box>
