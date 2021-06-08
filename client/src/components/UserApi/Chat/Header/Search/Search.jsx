7dcbec2e2d33a68ce6eb064deb76f16e3a274fec
0x d83f <alex.borrovikov@gmail.com>
2021-06-08 12:46:01 +0300

    add modal window for adding chats in sidebar

--- client/src/components/UserApi/Chat/Header/Search/Search.jsx
+++ client/src/components/UserApi/Chat/Header/Search/Search.jsx
@@ -1,25 +1,86 @@
-import React from 'react';
-import {Box, IconButton} from "@material-ui/core";
+import React, {useState} from 'react';
+import {Box, Button, Grid, IconButton} from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import AddIcon from "@material-ui/icons/Add";
-import useStyles from "../Header.styles";
+import useHeaderStyles from "../Header.styles";
+import useStyles from "./Search.styles";
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 import clsx from "clsx";
 
+
 const Search = () => {
-    const headerClasses = useStyles();
+    const headerClasses = useHeaderStyles();
+    const classes = useStyles();
+    const [open, setOpen] = useState(false);
+    const [age, setAge] = React.useState('');
+
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
     return (
         <Box display='flex' alignItems='center' justifyContent='space-between' className={clsx(headerClasses.borderRight,headerClasses.padding)}>
-            <div >
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                    <div className={classes.paper}>
+                        <Grid>
+                            <FormControl className={classes.formControl}>
+                                <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={age}
+                                    onChange={handleChange}
+                                >
+                                    <MenuItem value={10}>Ten</MenuItem>
+                                    <MenuItem value={20}>Twenty</MenuItem>
+                                    <MenuItem value={30}>Thirty</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid>
+                            <Button className={classes.buttonAdd} variant="contained" color="primary">
+                                Add
+                            </Button>
+                        </Grid>
+                    </div>
+                </Fade>
+            </Modal>
+            <Grid >
                 <input type="text" className={headerClasses.input}/>
-            </div>
-            <div >
+            </Grid>
+            <Grid >
                 <IconButton>
                     <SearchIcon />
                 </IconButton>
-                <IconButton>
+                <IconButton onClick={handleOpen}>
                     <AddIcon />
                 </IconButton>
-            </div>
+            </Grid>
         </Box>
     );
 };
--- client/src/components/UserApi/Chat/Header/Search/Search.styles.js
+++ client/src/components/UserApi/Chat/Header/Search/Search.styles.js
@@ -1,9 +1,30 @@
 import {makeStyles} from "@material-ui/core";
 
 
-const useStyles = makeStyles({
-    root: {
-
+const useStyles = makeStyles(theme => ({
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-})
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 300,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+    buttonAdd: {
+        width: '100%',
+        background: '#4B83F0',
+        '&:hover': {
+            background: '#2762D4'
+        }
+    }
+}))
 export default useStyles
